0ffe07741132fe2b455cf01cd02f4e29a88cdf25
Jean Lanziloti <jean.lanziloti@gmail.com>
2012-09-13 16:47:28 -0300

    Projeto do curso de Ruby on Rails - Caelum

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+		def create
+			@job = Job.find(params[:job_id])
+			@comment = @job.comments.build(params[:comment])
+			@comment.save
+			redirect_to @job
+
+		end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBoard::Application.config.secret_token = '2f786b7c3f49bca6c6a37869eb4374d4d140aa167a0ff93a3ec94d88673f0e404a7d9a66ec770df802b90897c437b67a733372ab4a24030dd27f6ff1fce0490c'
